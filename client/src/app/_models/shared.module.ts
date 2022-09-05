ac9a1c2ca03feb79617050efd0e4aafa90a81dcc
WadiiMakhloufUit <wadiimakhlouf.uit@gmail.com>
2022-09-05 11:17:06 +0100

    section 12

--- client/src/app/_models/shared.module.ts
+++ client/src/app/_models/shared.module.ts
@@ -5,6 +5,7 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import {FileUploadModule} from 'ng2-file-upload';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 
 
 
@@ -19,6 +20,7 @@ import {FileUploadModule} from 'ng2-file-upload';
     TabsModule.forRoot(),
     NgxGalleryModule,
     FileUploadModule,
+    BsDatepickerModule.forRoot()
 
   ],
 
@@ -27,7 +29,8 @@ import {FileUploadModule} from 'ng2-file-upload';
     ToastrModule,
     TabsModule,
     NgxGalleryModule,
-    FileUploadModule
+    FileUploadModule,
+    BsDatepickerModule
   ]
 })
 export class SharedModule { }
